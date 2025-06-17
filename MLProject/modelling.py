bba28013787830a4b04eaecce61e84ec0606c9bf
ItsNudle <115250029+ItsNudle@users.noreply.github.com>
2025-06-17 20:37:18 +0700

    Model ML

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -21,7 +21,7 @@ X = pd.read_csv("spam_ham_emails_preprocessing/tfidf.csv")
 y = pd.read_csv("spam_ham_emails_preprocessing/labels.csv")["label"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-with mlflow.start_run(nested=True):
+with mlflow.start_run():
     model = LogisticRegression()
     model.fit(X_train, y_train)
     acc = accuracy_score(y_test, model.predict(X_test))
